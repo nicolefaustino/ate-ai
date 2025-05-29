31171e8e54d3dfe8c02c4dbb8828cf1b702857d0
dizondroid <djelmey04@gmail.com>
2025-05-29 23:44:03 +0800

    firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCFqnNvxCCaQwE6iEbgM4Dt6UkNJ9O2O1U",
+  authDomain: "ate-ai.firebaseapp.com",
+  projectId: "ate-ai",
+  storageBucket: "ate-ai.firebasestorage.app",
+  messagingSenderId: "450557645720",
+  appId: "1:450557645720:web:c5a672df932fc364e991ed",
+  measurementId: "G-VFN7539SX4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export default app;
